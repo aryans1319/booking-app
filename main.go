e918dadb5315c2e9135b6b6842b6cc6253dbd996
aryans1319 <aryan4p130202@gmail.com>
2022-12-21 13:38:11 +0530

    issue: data type fixed

--- main.go
+++ main.go
@@ -15,4 +15,40 @@ func main (){
 	// Another way to print
 	fmt.Printf("Welcome here to %v my booking application\n", conferenceName)
 	fmt.Printf("we have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+
+	// Data types in Go
+	/*
+		[Strings, Integers, Boolean, Maps, Arrays]
+		Go can infer a type when we assign a value to 
+		any variable 
+		Eg: const totalTickets = 50
+			var name = "Aryan"
+		But, if we don't assign a value to a variable we need
+		to define the data type	
+		Eg: var userName string
+			var totalTickets int
+	*/
+	var userName string
+	var totalTickets int
+
+	userName = "Aryan"
+	totalTickets = 50
+
+	fmt.Printf("User %v booked %v tickets for the conference\n", userName, totalTickets);
+
+	/*
+		Advantage of GO: 
+			discover mistakes at compile time and not at run time
+	*/
+
+	// Now how to check the data-type of a variable in Go
+
+	fmt.Printf("Conference Tickets is %T, Username is %T\n", conferenceTickets, userName)
+
+	// Alternative way to write this
+	//	var myName string = "Aryan"
+	myName := "Aryan"; 
+	// this way doesn't work for constants and when we explicitly want to 
+	// declare a var for eg: var remainingTickets uint = 50
+	fmt.Printf("%v", myName)
 }
